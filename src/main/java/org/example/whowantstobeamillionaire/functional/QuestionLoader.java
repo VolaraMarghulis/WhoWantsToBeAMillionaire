efb0254cca283293d9dd103acdb6ade25487f3c6
Volara <valeriu.juc1@gmail.com>
2025-05-19 17:28:24 +0300

    add logger to check

--- src/main/java/org/example/whowantstobeamillionaire/functional/QuestionLoader.java
+++ src/main/java/org/example/whowantstobeamillionaire/functional/QuestionLoader.java
@@ -1,12 +1,18 @@
 package org.example.whowantstobeamillionaire.functional;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.InputStream;
 
 public class QuestionLoader {
     public static Question[] loadQuestions(String resourcePath) {
         try (InputStream is = QuestionLoader.class.getResourceAsStream(resourcePath)) {
+            // <----- log to check if it can read
+            System.out.println(is == null ? "InputStream is null!" : "InputStream is OK!");
             ObjectMapper mapper = new ObjectMapper();
-            return mapper.readValue(is, Question[].class);
+            Question[] arr = mapper.readValue(is, Question[].class);
+            // <----- log to view length of questions
+            System.out.println("Loaded " + arr.length + " questions.");
+            return arr;
         } catch (Exception e) {
             e.printStackTrace();
             return new Question[0];
